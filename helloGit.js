893cd44e0577186f0087e6f254627732683f893a
Zineng Guo <zinengguo97@gmail.com>
2019-07-05 16:23:41 +0800

    Update helloGit.js on GitHub

--- helloGit.js
+++ helloGit.js
@@ -1,7 +1,6 @@
-console.log('Hello Git!!!');
-console.log('Hello GitHub');
+console.log('This is version on GitHub');
 var a = 8;
 var b = 7;
-if (a > b) {
-  console.log('a > b');
+if (a < b) {
+  console.log('a < b');
 }
